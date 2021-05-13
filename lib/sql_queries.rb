1d9c15122f75c9ffc6237a7f2674b45abdd327de
Lena Hou <lenahou2013@gmail.com>
2021-05-13 19:54:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -47,7 +47,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, SUM(pledges.amount) FROM category
   INNER JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY SUM(pledges.amount) HAVING projects.category = "music"; 
+  GROUP BY SUM(pledges.amount) HAVING projects.category = "music";
   "
 end
 
